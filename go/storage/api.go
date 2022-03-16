e08cadf3d8c02cb171beba1e2f2191d639319b2b
ennsharma <ennsharma@oasislabs.com>
2022-03-16 17:36:41 -0400

    Fill in storage API

---
+++ go/storage/api.go
@@ -0,0 +1,111 @@
+// Package storage defines storage interfaces.
+package storage
+
+import (
+	beacon "github.com/oasisprotocol/oasis-core/go/beacon/api"
+	"github.com/oasisprotocol/oasis-core/go/common"
+	consensus "github.com/oasisprotocol/oasis-core/go/consensus/api"
+	"github.com/oasisprotocol/oasis-core/go/consensus/api/transaction"
+	"github.com/oasisprotocol/oasis-core/go/consensus/api/transaction/results"
+	governance "github.com/oasisprotocol/oasis-core/go/governance/api"
+	registry "github.com/oasisprotocol/oasis-core/go/registry/api"
+	scheduler "github.com/oasisprotocol/oasis-core/go/scheduler/api"
+	staking "github.com/oasisprotocol/oasis-core/go/staking/api"
+)
+
+// SourceStorage defines an interface for retrieving raw block data.
+type SourceStorage interface {
+
+	// GetBlockData gets block data at the specified height. This includes all
+	// block header information, as well as transactions and events included
+	// within that block.
+	GetBlockData(height int64) (*BlockData, error)
+
+	// GetBeaconData gets beacon data at the specified height. This includes
+	// the epoch number at that height, as well as the beacon state.
+	GetBeaconData(height int64) (*BeaconData, error)
+
+	// GetRegistryData gets registry data at the specified height. This includes
+	// all registered entities and their controlled nodes and statuses.
+	GetRegistryData(height int64) (*RegistryData, error)
+
+	// GetStakingData gets staking data at the specified height. This includes
+	// staking backend events to be applied to indexed state.
+	GetStakingData(height int64) (*StakingData, error)
+
+	// GetSchedulerData gets scheduler data at the specified height. This
+	// includes all validators and runtime committees.
+	GetSchedulerData(height int64) (*SchedulerData, error)
+
+	// GetGovernanceData gets governance data at the specified height. This
+	// includes all proposals, their respective statuses and voting responses.
+	GetGovernanceData(height int64) (*GovernanceData, error)
+
+	// TODO: Extend this interface to include a GetRoothashData to pull
+	// runtime blocks. This is only relevant when we begin to build runtime
+	// analyzers.
+
+	// Name returns the name of this source storage.
+	Name() string
+}
+
+// BlockData represents data for a block at a given height.
+type BlockData struct {
+	BlockHeader  *consensus.Block
+	Transactions []*transaction.SignedTransaction
+	Results      []*results.Result
+}
+
+// BeaconData represents data for the random beacon at a given height.
+type BeaconData struct {
+	Epoch  beacon.EpochTime
+	Beacon []byte
+}
+
+// RegistryData represents data for the node registry at a given height.
+//
+// Note: The registry backend supports getting events directly. We support
+// retrieving events as updates to apply when getting data at specific height.
+type RegistryData struct {
+	RuntimeEvents     []*registry.RuntimeEvent
+	EntityEvents      []*registry.EntityEvent
+	NodeEvent         []*registry.NodeEvent
+	NodeUnfrozenEvent []*registry.NodeUnfrozenEvent
+}
+
+// StakingData represents data for accounts at a given height.
+//
+// Note: The staking backend supports getting events directly. We support
+// retrieving events as updates to apply when getting data at specific height.
+type StakingData struct {
+	Transfers        []*staking.TransferEvent
+	Burns            []*staking.BurnEvent
+	Escrows          []*staking.EscrowEvent
+	AllowanceChanges []*staking.AllowanceChangeEvent
+}
+
+// SchedulerData represents data for elected committees and validators at a given height.
+type SchedulerData struct {
+	Validators []*scheduler.Validator
+	Committees map[common.Namespace][]*scheduler.Committee
+}
+
+// GovernanceData represents governance data for proposals at a given height.
+//
+// Note: The governance backend supports getting events directly. We support
+// retrieving events as updates to apply when getting data at a specific height.
+type GovernanceData struct {
+	ProposalSubmissions   []*governance.ProposalSubmittedEvent
+	ProposalExecutions    []*governance.ProposalExecutedEvent
+	ProposalFinalizations []*governance.ProposalFinalizedEvent
+	Votes                 []*governance.VoteEvent
+}
+
+// TargetStorage defines an interface for reading and writing
+// processed block data.
+type TargetStorage interface {
+	// TODO: Define the rest of this interface.
+
+	// Name returns the name of this target storage.
+	Name() string
+}
