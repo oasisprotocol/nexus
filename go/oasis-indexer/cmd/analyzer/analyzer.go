19af0d1a91dd5f6fc6b08f7cbdc46c3bc12b4a1a
Xi Zhang <xizhang@oasislabs.com>
2022-03-30 16:32:50 -0500

    Use cli for network

--- go/oasis-indexer/cmd/analyzer/analyzer.go
+++ go/oasis-indexer/cmd/analyzer/analyzer.go
@@ -4,13 +4,17 @@ package analyzer
 import (
 	"context"
 	"errors"
+	"fmt"
 	"os"
+	"strconv"
 
+	"github.com/oasisprotocol/oasis-sdk/client-sdk/go/config"
 	"github.com/spf13/cobra"
 
 	"github.com/oasislabs/oasis-block-indexer/go/log"
 	"github.com/oasislabs/oasis-block-indexer/go/oasis-indexer/cmd/common"
 	"github.com/oasislabs/oasis-block-indexer/go/storage"
+	"github.com/oasislabs/oasis-block-indexer/go/storage/oasis"
 )
 
 const (
@@ -27,16 +31,34 @@ var (
 	analyzeCmd = &cobra.Command{
 		Use:   "analyze",
 		Short: "Analyze blocks",
+		Args:  cobra.ExactArgs(6),
 		Run:   runAnalyzer,
 	}
 )
 
 func runAnalyzer(cmd *cobra.Command, args []string) {
+	_, chainContext, rpc, description, symbol, decimals := args[0], args[1], args[2], args[3], args[4], args[5]
+
+	network := config.Network{
+		ChainContext: chainContext,
+		RPC:          rpc,
+	}
+
+	decimalsInt, err := strconv.ParseUint(decimals, 10, 8)
+	cobra.CheckErr(err)
+
+	network.Description = description
+	network.Denomination.Symbol = symbol
+	network.Denomination.Decimals = uint8(decimalsInt)
+
 	common.Init()
 
-	_, err := NewAnalyzer()
+	analyzer, err := NewAnalyzer(network)
+	cobra.CheckErr(err)
+
 	switch {
 	case err == nil:
+		analyzer.Start()
 	case errors.Is(err, context.Canceled):
 		// Shutdown requested during startup.
 		return
@@ -47,22 +69,52 @@ func runAnalyzer(cmd *cobra.Command, args []string) {
 
 // Analyzer is the Oasis Indexer's analysis service.
 type Analyzer struct {
+	Network       config.Network
 	TargetStorage storage.TargetStorage
-
-	logger *log.Logger
+	logger        *log.Logger
 }
 
 // NewAnalyzer creates and starts a new Analyzer
-func NewAnalyzer() (*Analyzer, error) {
+func NewAnalyzer(net config.Network) (*Analyzer, error) {
 	logger := common.Logger().WithModule(moduleName)
 
 	analyzer := &Analyzer{
-		logger: logger,
+		logger:  logger,
+		Network: net,
 	}
 
 	logger.Info("Starting oasis-indexer analysis layer.")
 
 	return analyzer, nil
+}
+
+func (analyzer *Analyzer) Start() {
+	c := context.Background()
+	client, err := oasis.NewOasisNodeClient(c, &analyzer.Network)
+	document, err := client.GenesisDocument(c)
+	cobra.CheckErr(err)
+	initialHeight := document.Height
+	height := initialHeight
+
+	for height < (initialHeight + 10) {
+		fmt.Println(height)
+		blockData, err := client.BlockData(c, height)
+		cobra.CheckErr(err)
+		fmt.Println(blockData)
+		registryData, err := client.RegistryData(c, height)
+		cobra.CheckErr(err)
+		fmt.Println(registryData)
+		stakingData, err := client.StakingData(c, height)
+		cobra.CheckErr(err)
+		fmt.Println(stakingData)
+		schedulerData, err := client.SchedulerData(c, height)
+		cobra.CheckErr(err)
+		fmt.Println(schedulerData)
+		governanceData, err := client.GovernanceData(c, height)
+		cobra.CheckErr(err)
+		fmt.Println(governanceData)
+		height += 1
+	}
 }
 
 // Register registers the process sub-command.
