f51ac24e784ddca2303a6cfca45350a5a917aa3a
Xi Zhang <xizhang@oasislabs.com>
2022-03-30 16:32:50 -0500

    Use config yaml for network start

--- go/oasis-indexer/cmd/analyzer/analyzer.go
+++ go/oasis-indexer/cmd/analyzer/analyzer.go
@@ -4,25 +4,32 @@ package analyzer
 import (
 	"context"
 	"errors"
+	"fmt"
+	"io/ioutil"
 	"os"
 
+	"github.com/oasisprotocol/oasis-sdk/client-sdk/go/config"
 	"github.com/spf13/cobra"
+	"gopkg.in/yaml.v2"
 
 	"github.com/oasislabs/oasis-block-indexer/go/log"
 	"github.com/oasislabs/oasis-block-indexer/go/oasis-indexer/cmd/common"
 	"github.com/oasislabs/oasis-block-indexer/go/storage"
+	"github.com/oasislabs/oasis-block-indexer/go/storage/oasis"
 )
 
 const (
 	// CfgStorageEndpoint is the flag for setting the connection string to
 	// the backing storage.
 	CfgStorageEndpoint = "storage.endpoint"
+	CfgNetworkFile     = "network.config"
 
 	moduleName = "analysis"
 )
 
 var (
 	cfgStorageEndpoint string
+	cfgNetworkFile     string
 
 	analyzeCmd = &cobra.Command{
 		Use:   "analyze",
@@ -32,11 +39,20 @@ var (
 )
 
 func runAnalyzer(cmd *cobra.Command, args []string) {
+	rawCfg, err := ioutil.ReadFile(cfgNetworkFile)
+	cobra.CheckErr(err)
+
+	var network config.Network
+	yaml.Unmarshal([]byte(rawCfg), &network)
+
 	common.Init()
 
-	_, err := NewAnalyzer()
+	analyzer, err := NewAnalyzer(network)
+	cobra.CheckErr(err)
+
 	switch {
 	case err == nil:
+		analyzer.Start()
 	case errors.Is(err, context.Canceled):
 		// Shutdown requested during startup.
 		return
@@ -47,17 +63,18 @@ func runAnalyzer(cmd *cobra.Command, args []string) {
 
 // Analyzer is the Oasis Indexer's analysis service.
 type Analyzer struct {
+	Network       config.Network
 	TargetStorage storage.TargetStorage
-
-	logger *log.Logger
+	logger        *log.Logger
 }
 
 // NewAnalyzer creates and starts a new Analyzer
-func NewAnalyzer() (*Analyzer, error) {
+func NewAnalyzer(net config.Network) (*Analyzer, error) {
 	logger := common.Logger().WithModule(moduleName)
 
 	analyzer := &Analyzer{
-		logger: logger,
+		logger:  logger,
+		Network: net,
 	}
 
 	logger.Info("Starting oasis-indexer analysis layer.")
@@ -65,9 +82,38 @@ func NewAnalyzer() (*Analyzer, error) {
 	return analyzer, nil
 }
 
+func (analyzer *Analyzer) Start() {
+	c := context.Background()
+	client, err := oasis.NewOasisNodeClient(c, &analyzer.Network)
+	document, err := client.GenesisDocument(c)
+	cobra.CheckErr(err)
+	initialHeight := document.Height
+	height := initialHeight + 1
+
+	for height < (initialHeight + 10) {
+		fmt.Println(height)
+		blockData, err := client.BlockData(c, height)
+		cobra.CheckErr(err)
+		fmt.Println(blockData)
+		registryData, err := client.RegistryData(c, height)
+		cobra.CheckErr(err)
+		fmt.Println(registryData)
+		stakingData, err := client.StakingData(c, height)
+		cobra.CheckErr(err)
+		fmt.Println(stakingData)
+		schedulerData, err := client.SchedulerData(c, height)
+		cobra.CheckErr(err)
+		fmt.Println(schedulerData)
+		governanceData, err := client.GovernanceData(c, height)
+		cobra.CheckErr(err)
+		fmt.Println(governanceData)
+		height += 1
+	}
+}
+
 // Register registers the process sub-command.
 func Register(parentCmd *cobra.Command) {
 	analyzeCmd.Flags().StringVar(&cfgStorageEndpoint, CfgStorageEndpoint, "", "a postgresql-compliant connection url")
-
+	analyzeCmd.Flags().StringVar(&cfgNetworkFile, CfgNetworkFile, "", "path to a network configuration file")
 	parentCmd.AddCommand(analyzeCmd)
 }
