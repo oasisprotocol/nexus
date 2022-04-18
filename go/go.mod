1fcd384a644317b0c61e0100d58628c30b6fbd12
ennsharma <ennsharma@oasislabs.com>
2022-04-18 18:23:23 -0400

    Initialize API layer shim

--- go/go.mod
+++ go/go.mod
@@ -5,8 +5,10 @@ go 1.15
 require (
 	github.com/fxamacker/cbor/v2 v2.4.0 // indirect
 	github.com/go-kit/log v0.2.0
+	github.com/gorilla/mux v1.7.3
 	github.com/hashicorp/go-hclog v1.1.0 // indirect
 	github.com/hashicorp/go-plugin v1.4.3 // indirect
+	github.com/jackc/pgx/v4 v4.15.0
 	github.com/libp2p/go-libp2p v0.18.0 // indirect
 	github.com/libp2p/go-libp2p-pubsub v0.6.1 // indirect
 	github.com/oasisprotocol/deoxysii v0.0.0-20220228165953-2091330c22b7 // indirect
@@ -18,9 +20,10 @@ require (
 	github.com/spf13/pflag v1.0.5
 	github.com/spf13/viper v1.10.1
 	github.com/tendermint/tendermint v0.34.15 // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
 	golang.org/x/sys v0.0.0-20220227234510-4e6760a101f9 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/grpc v1.45.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0
 )
--- go/oasis-indexer/cmd/api/api.go
+++ go/oasis-indexer/cmd/api/api.go
@@ -2,25 +2,122 @@
 package api
 
 import (
+	"context"
+	"errors"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/gorilla/mux"
 	"github.com/spf13/cobra"
 
+	"github.com/oasislabs/oasis-block-indexer/go/api"
+	consensusApi "github.com/oasislabs/oasis-block-indexer/go/api/consensus"
+	"github.com/oasislabs/oasis-block-indexer/go/log"
 	"github.com/oasislabs/oasis-block-indexer/go/oasis-indexer/cmd/common"
 )
 
-var apiCmd = &cobra.Command{
-	Use:   "serve",
-	Short: "Serve Oasis Indexer API",
-	Run:   runServer,
-}
+const (
+	// CfgServiceEndpoint is the service endpoint at which the Oasis Indexer API
+	// can be reached.
+	CfgServiceEndpoint = "api.service_endpoint"
+
+	moduleName = "api"
+)
+
+var (
+	cfgServiceEndpoint string
+
+	apiCmd = &cobra.Command{
+		Use:   "serve",
+		Short: "Serve Oasis Indexer API",
+		Run:   runServer,
+	}
+)
 
 func runServer(cmd *cobra.Command, args []string) {
-	common.Init()
+	if err := common.Init(); err != nil {
+		os.Exit(1)
+	}
 
-	logger := common.Logger()
-	logger.Info("Starting api layer.")
+	service, err := NewAPIService()
+	switch {
+	case err == nil:
+		service.Start()
+	case errors.Is(err, context.Canceled):
+		// Shutdown requested during startup.
+		return
+	default:
+		common.Logger().Error("service failed to start",
+			"error", err,
+		)
+		os.Exit(1)
+	}
+}
+
+// APIService is the Oasis Indexer's API service.
+type APIService struct {
+	server string
+	router *mux.Router
+	logger *log.Logger
+}
+
+// NewAPIService creates a new API service.
+func NewAPIService() (*APIService, error) {
+	logger := common.Logger().WithModule(moduleName)
+
+	baseRouter := mux.NewRouter()
+	apiRouter := baseRouter.PathPrefix("/api").Subrouter()
+
+	// Register endpoints.
+	apiRouter.HandleFunc("/", api.GetMetadata).Methods("GET")
+	apiRouter.HandleFunc("/consensus/blocks", consensusApi.GetBlocks).Methods("GET")
+	apiRouter.HandleFunc("/consensus/blocks/{height}", consensusApi.GetBlock).Methods("GET")
+	apiRouter.HandleFunc("/consensus/blocks/{height}/beacon", consensusApi.GetBeacon).Methods("GET")
+	apiRouter.HandleFunc("/consensus/blocks/{height}/events", consensusApi.GetBlockEvents).Methods("GET")
+	apiRouter.HandleFunc("/consensus/transactions", consensusApi.GetTransactions).Methods("GET")
+	apiRouter.HandleFunc("/consensus/transactions/{tx_hash}", consensusApi.GetTransaction).Methods("GET")
+	apiRouter.HandleFunc("/consensus/transactions/{tx_hash}/events", consensusApi.GetTransactionEvents).Methods("GET")
+	apiRouter.HandleFunc("/consensus/epochs", consensusApi.GetEpochs).Methods("GET")
+	apiRouter.HandleFunc("/consensus/epochs/{epoch}", consensusApi.GetEpoch).Methods("GET")
+	apiRouter.HandleFunc("/consensus/epochs/{epoch}/validators", consensusApi.GetValidators).Methods("GET")
+	apiRouter.HandleFunc("/consensus/epochs/{epoch}/committees/{runtime_id}", consensusApi.GetCommittees).Methods("GET")
+	apiRouter.HandleFunc("/consensus/proposals", consensusApi.GetProposals).Methods("GET")
+	apiRouter.HandleFunc("/consensus/proposals/{proposal_id}", consensusApi.GetProposal).Methods("GET")
+	apiRouter.HandleFunc("/consensus/entities", consensusApi.GetEntities).Methods("GET")
+	apiRouter.HandleFunc("/consensus/entities/{entity_id}", consensusApi.GetEntity).Methods("GET")
+	apiRouter.HandleFunc("/consensus/entities/{entity_id}/nodes", consensusApi.GetEntityNodes).Methods("GET")
+	apiRouter.HandleFunc("/consensus/entities/{entity_id}/nodes/{node_id}", consensusApi.GetEntityNode).Methods("GET")
+	apiRouter.HandleFunc("/consensus/accounts", consensusApi.GetAccounts).Methods("GET")
+	apiRouter.HandleFunc("/consensus/accounts/{address}", consensusApi.GetAccount).Methods("GET")
+	apiRouter.HandleFunc("/status", consensusApi.GetStatus).Methods("GET")
+
+	return &APIService{
+		server: cfgServiceEndpoint,
+		router: apiRouter,
+		logger: logger,
+	}, nil
+}
+
+// Start starts the API service.
+func (s *APIService) Start() {
+	s.logger.Info("starting api service")
+
+	server := &http.Server{
+		Addr:           s.server,
+		Handler:        s.router,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	s.logger.Error("shutting down",
+		"error", server.ListenAndServe(),
+	)
 }
 
 // Register registers the process sub-command.
 func Register(parentCmd *cobra.Command) {
+	apiCmd.Flags().StringVar(&cfgServiceEndpoint, CfgServiceEndpoint, "", "service endpoint from which to serve indexer api")
 	parentCmd.AddCommand(apiCmd)
 }
