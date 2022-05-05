8eeb44ac3a99c171ad3d0300c08f6c07d88def87
ennsharma <ennsharma@oasislabs.com>
2022-05-05 09:08:13 -0400

    unstage bad changes

--- oasis-indexer/cmd/healthcheck/healthcheck.go
+++
@@ -1,4 +0,0 @@
-// Package healthcheck implements the `healthcheck` sub-command. This is intended
-// to serve as a status check of indexer progress, to validate that the
-// current status of the indexer is as expected.
-package healthcheck
