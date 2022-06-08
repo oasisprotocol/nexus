ed5e39221fdb2ebfaa948ff167cd8748a5cdafba
Nikhil Sharma <ennsharma@oasislabs.com>
2022-06-08 19:27:48 -0400

    fix: Implement log test suite (#70)

--- log/log.go
+++ log/log.go
@@ -87,14 +87,8 @@ func (l *Logger) Error(msg string, keyvals ...interface{}) {
 func (l *Logger) With(keyvals ...interface{}) *Logger {
 	return &Logger{
 		logger: log.With(l.logger, keyvals...),
-	}
-}
-
-// WithPrefix returns a clone of the logger with the provided key/value pairs
-// added as context for all subsequent logs.
-func (l *Logger) WithPrefix(keyvals ...interface{}) *Logger {
-	return &Logger{
-		logger: log.WithPrefix(l.logger, keyvals...),
+		level:  l.level,
+		module: l.module,
 	}
 }
 
