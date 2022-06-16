c1bf5643d55a76db20f3c03e85c6e882319b7279
ennsharma <ennsharma@oasislabs.com>
2022-06-16 15:20:50 -0400

    storage: Add oasis client test

---
+++ storage/oasis/client_test.go
@@ -0,0 +1,173 @@
+package oasis
+
+import (
+	"context"
+	"os"
+	"testing"
+
+	config "github.com/oasisprotocol/oasis-sdk/client-sdk/go/config"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+const (
+	pastHeight   = 8048956      // oasis-3 genesis height
+	futureHeight = 100000000000 // an unreasonably high height
+)
+
+func newClient() (*Client, error) {
+	network := &config.Network{
+		ChainContext: os.Getenv("CI_TEST_CHAIN_CONTEXT"),
+		RPC:          os.Getenv("CI_TEST_NODE_RPC"),
+	}
+	return NewClient(context.Background(), network)
+}
+
+func TestConnect(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping testing in short mode")
+	}
+
+	_, err := newClient()
+	require.Nil(t, err)
+}
+
+func TestInvalidConnect(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping testing in short mode")
+	}
+
+	network := &config.Network{
+		ChainContext: os.Getenv("CI_TEST_CHAIN_CONTEXT"),
+		RPC:          "an invalid rpc endpoint",
+	}
+	_, err := NewClient(context.Background(), network)
+	require.NotNil(t, err)
+
+	network = &config.Network{
+		ChainContext: "an invalid chaincontext",
+		RPC:          os.Getenv("CI_TEST_NODE_RPC"),
+	}
+	_, err = NewClient(context.Background(), network)
+	require.NotNil(t, err)
+}
+
+func TestGenesisDocument(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping testing in short mode")
+	}
+
+	ctx := context.Background()
+
+	client, err := newClient()
+	require.Nil(t, err)
+
+	_, err = client.GenesisDocument(ctx)
+	assert.Nil(t, err)
+}
+
+func TestBlockData(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping testing in short mode")
+	}
+
+	ctx := context.Background()
+
+	client, err := newClient()
+	require.Nil(t, err)
+
+	_, err = client.BlockData(ctx, pastHeight)
+	require.Nil(t, err)
+
+	_, err = client.BlockData(ctx, futureHeight)
+	require.NotNil(t, err)
+}
+
+func TestBeaconData(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping testing in short mode")
+	}
+
+	ctx := context.Background()
+
+	client, err := newClient()
+	require.Nil(t, err)
+
+	_, err = client.BeaconData(ctx, pastHeight)
+	require.NotNil(t, err)
+	// ^beacon data was not available at this height
+
+	_, err = client.BeaconData(ctx, futureHeight)
+	require.Nil(t, err)
+	// ^beacon value is known for future heights
+}
+
+func TestRegistryData(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping testing in short mode")
+	}
+
+	ctx := context.Background()
+
+	client, err := newClient()
+	require.Nil(t, err)
+
+	_, err = client.RegistryData(ctx, pastHeight)
+	require.Nil(t, err)
+
+	_, err = client.RegistryData(ctx, futureHeight)
+	require.NotNil(t, err)
+}
+
+func TestStakingData(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping testing in short mode")
+	}
+
+	ctx := context.Background()
+
+	client, err := newClient()
+	require.Nil(t, err)
+
+	_, err = client.StakingData(ctx, pastHeight)
+	require.Nil(t, err)
+
+	_, err = client.StakingData(ctx, futureHeight)
+	require.NotNil(t, err)
+}
+
+func TestSchedulerData(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping testing in short mode")
+	}
+
+	ctx := context.Background()
+
+	client, err := newClient()
+	require.Nil(t, err)
+
+	_, err = client.SchedulerData(ctx, pastHeight)
+	require.Nil(t, err)
+
+	_, err = client.SchedulerData(ctx, futureHeight)
+	require.Nil(t, err)
+	// ^the scheduler returns the list of current validators
+	// for future heights as well, so this does not fail
+}
+
+func TestGovernanceData(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping testing in short mode")
+	}
+
+	ctx := context.Background()
+
+	client, err := newClient()
+	require.Nil(t, err)
+
+	_, err = client.GovernanceData(ctx, pastHeight)
+	require.Nil(t, err)
+
+	_, err = client.GovernanceData(ctx, futureHeight)
+	require.NotNil(t, err)
+}
