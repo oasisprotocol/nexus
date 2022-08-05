286bc0f1420dbc6a6e789a34b8ff58116625f65b
ennsharma <ennsharma@oasislabs.com>
2022-08-05 10:22:52 -0700

    ignore lint on single case switch

--- cmd/analyzer/analyzer.go
+++ cmd/analyzer/analyzer.go
@@ -125,7 +125,9 @@ func NewService(cfg *config.AnalysisConfig) (*Service, error) {
 	// Initialize analyzers.
 	analyzers := map[string]analyzer.Analyzer{}
 	for _, analyzerCfg := range cfg.Analyzers {
-		if analyzerCfg.Name == "consensus_main_damask" {
+		//nolint:gocritic
+		switch analyzerCfg.Name {
+		case "consensus_main_damask":
 			consensusMainDamask, err := consensus.NewMain(analyzerCfg, client, logger)
 			if err != nil {
 				return nil, err
