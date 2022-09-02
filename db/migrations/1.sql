df12b7d881da710450e8e3c4475a54b84b4caeb2
Warren He <w@oasislabs.com>
2022-09-02 15:46:25 -0700

    friday

--- db/migrations/1.sql
+++ db/migrations/1.sql
@@ -1,7 +1,13 @@
 CREATE TABLE block_extra (
     chain_alias VARCHAR(32),
-    round BIGINT,
+    height BIGINT,
     gas_used BIGINT,
     size INTEGER
 );
-CREATE UNIQUE INDEX block_extra_chain_alias_round ON block_extra (chain_alias, round);
+CREATE UNIQUE INDEX block_extra_chain_alias_height ON block_extra (chain_alias, height);
+
+CREATE TABLE progress (
+    chain_alias VARCHAR(32) PRIMARY KEY,
+    first_unscanned_height BIGINT
+);
+INSERT INTO progress (chain_alias, first_unscanned_height) VALUES ('mainnet_emerald', 2715686);
--- work/main.go
+++ work/main.go
@@ -17,32 +17,21 @@ import (
 	"google.golang.org/grpc/credentials"
 )
 
-func mainFallible(ctx context.Context) error {
-	conn, err := commonGrpc.Dial("grpc.oasis.dev:443", grpc.WithTransportCredentials(credentials.NewTLS(&tls.Config{})))
-	if err != nil {
-		return err
-	}
-	var rtid common.Namespace
-	chainAlias := "mainnet_emerald"
-	if err = rtid.UnmarshalHex("000000000000000000000000000000000000000000000000e2eaa99fc008f87f"); err != nil {
-		return err
-	}
-	mainnetEmeraldContext := signature.DeriveChainContext(rtid, "b11b369e0da5bb230b220127f5e7b242d385ef8c6f54906243f30af63c815535")
-	rc := client.New(conn, rtid)
-	round := uint64(2679238)
+func scanRound(ctx context.Context, dbConn *pgx.Conn, chainAlias string, rtClient client.RuntimeClient, sigContext signature.Context, round int64) error {
+	fmt.Printf("scanning round %d\n", round)
 	var gasUsed int64
 	var size int
 	// Inaccurate: Ignore unparseable transactions.
-	txrs, err := rc.GetTransactionsWithResults(ctx, round)
+	txrs, err := rtClient.GetTransactionsWithResults(ctx, uint64(round))
 	if err != nil {
-		return fmt.Errorf("get transactions with results round %d: %w", round, err)
+		return fmt.Errorf("get transactions with results: %w", err)
 	}
 	for i, txr := range txrs {
-		fmt.Printf("%#v\n", txr)
+		// fmt.Printf("%#v\n", txr)
 		var txGasUsed int64
 		foundGasUsedEvent := false
 		for j, event := range txr.Events {
-			fmt.Printf("%#v\n", event)
+			// fmt.Printf("%#v\n", event)
 			coreEvents, err1 := core.DecodeEvent(event)
 			if err1 != nil {
 				return fmt.Errorf("decode tx %d event %d: %w", i, j, err1)
@@ -64,7 +53,7 @@ func mainFallible(ctx context.Context) error {
 		if !foundGasUsedEvent {
 			if txr.Result.IsSuccess() || txr.Result.IsUnknown() {
 				// Treat as if it used all the gas.
-				tx, err1 := txr.Tx.Verify(mainnetEmeraldContext)
+				tx, err1 := txr.Tx.Verify(sigContext)
 				if err1 != nil {
 					// Should not be allowed to have a successful transaction without verification passing.
 					return fmt.Errorf("verify tx %d: %w", i, err1)
@@ -74,22 +63,58 @@ func mainFallible(ctx context.Context) error {
 				// Inaccurate: Treat as not using any gas.
 			}
 		}
-		fmt.Printf("gas used: %d\n", txGasUsed)
+		// fmt.Printf("gas used: %d\n", txGasUsed)
 		gasUsed += txGasUsed
 		// Inaccurate: Re-serialize signed tx to estimate original size.
 		txSize := len(cbor.Marshal(txr.Tx))
-		fmt.Printf("tx size: %d\n", txSize)
+		// fmt.Printf("tx size: %d\n", txSize)
 		size += txSize
 	}
+	if err = dbConn.BeginTxFunc(ctx, pgx.TxOptions{IsoLevel: pgx.RepeatableRead}, func(tx pgx.Tx) error {
+		if _, err1 := dbConn.Exec(ctx, "INSERT INTO block_extra (chain_alias, height, gas_used, size) VALUES ($1, $2, $3, $4)", chainAlias, round, gasUsed, size); err1 != nil {
+			return err1
+		}
+		if _, err1 := dbConn.Exec(ctx, "UPDATE progress SET first_unscanned_height = $1 WHERE chain_alias = $2", round+1, chainAlias); err1 != nil {
+			return err1
+		}
+		return nil
+	}); err != nil {
+		return err
+	}
+	return nil
+}
+
+func scanLoop(ctx context.Context, dbConn *pgx.Conn, chainAlias string, rc client.RuntimeClient, sigContext signature.Context) error {
+	var firstUnscannedHeight int64
+	if err := dbConn.QueryRow(ctx, "SELECT first_unscanned_height FROM progress WHERE chain_alias = $1", chainAlias).Scan(&firstUnscannedHeight); err != nil {
+		return err
+	}
+	for round := firstUnscannedHeight; ; round++ {
+		if err := scanRound(ctx, dbConn, chainAlias, rc, sigContext, round); err != nil {
+			return fmt.Errorf("round %d: %w", round, err)
+		}
+	}
+}
+
+func mainFallible(ctx context.Context) error {
 	dbConn, err := pgx.Connect(ctx, "postgres://postgres:a@172.17.0.2/explorer")
 	if err != nil {
 		return err
 	}
-	rows, err := dbConn.Query(ctx, "INSERT INTO block_extra (chain_alias, round, gas_used, size) VALUES ($1, $2, $3, $4)", chainAlias, round, gasUsed, size)
+	conn, err := commonGrpc.Dial("grpc.oasis.dev:443", grpc.WithTransportCredentials(credentials.NewTLS(&tls.Config{})))
 	if err != nil {
 		return err
 	}
-	rows.Close()
+	chainAlias := "mainnet_emerald"
+	var rtid common.Namespace
+	if err = rtid.UnmarshalHex("000000000000000000000000000000000000000000000000e2eaa99fc008f87f"); err != nil {
+		return err
+	}
+	rc := client.New(conn, rtid)
+	sigContext := signature.DeriveChainContext(rtid, "b11b369e0da5bb230b220127f5e7b242d385ef8c6f54906243f30af63c815535")
+	if err = scanLoop(ctx, dbConn, chainAlias, rc, sigContext); err != nil {
+		return err
+	}
 	return nil
 }
 
