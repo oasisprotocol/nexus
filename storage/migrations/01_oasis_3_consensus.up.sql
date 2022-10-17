5e223cf6dc006332616762223934d213d96747f7
Xi Zhang <xizhang@oasislabs.com>
2022-10-17 10:31:54 -0500

    Remove foreign key constraint from events table
    - not all events belong an indexed Transaction

--- storage/migrations/01_oasis_3_consensus.up.sql
+++ storage/migrations/01_oasis_3_consensus.up.sql
@@ -58,9 +58,7 @@ CREATE TABLE oasis_3.events
 
   txn_block  BIGINT NOT NULL,
   txn_hash   TEXT NOT NULL,
-  txn_index  INTEGER,
-
-  FOREIGN KEY (txn_block, txn_hash, txn_index) REFERENCES oasis_3.transactions(block, txn_hash, txn_index)
+  txn_index  INTEGER
 );
 
 -- Beacon Backend Data
