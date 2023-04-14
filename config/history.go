87328a6712b4dd01349117616b9178ff275297d0
Warren He <w@oasislabs.com>
2023-04-14 14:16:38 -0700

    config: runtime round availability in history

--- config/history.go
+++ config/history.go
@@ -4,12 +4,19 @@ import (
 	"fmt"
 
 	consensus "github.com/oasisprotocol/oasis-core/go/consensus/api"
+	runtimeClient "github.com/oasisprotocol/oasis-core/go/runtime/client/api"
+
+	"github.com/oasisprotocol/oasis-indexer/common"
 )
 
 type Record struct {
 	ArchiveName   string `koanf:"archive_name"`
 	GenesisHeight int64  `koanf:"genesis_height"`
-	ChainContext  string `koanf:"chain_context"`
+	// RuntimeStartRounds has entries for runtimes that already exist at the
+	// genesis of this network. Look these up in the genesis document's
+	// .roothash.runtime_states[runtime_id_hex].round
+	RuntimeStartRounds map[common.Runtime]uint64 `koanf:"runtime_start_rounds"`
+	ChainContext       string                    `koanf:"chain_context"`
 }
 
 type History struct {
@@ -42,6 +49,25 @@ func (h *History) RecordForHeight(height int64) (*Record, error) {
 	)
 }
 
+func (h *History) RecordForRuntimeRound(runtime common.Runtime, round uint64) (*Record, error) {
+	if round == runtimeClient.RoundLatest {
+		return h.CurrentRecord(), nil
+	}
+	for _, r := range h.Records {
+		if round >= r.RuntimeStartRounds[runtime] {
+			return r, nil
+		}
+	}
+	earliestRecord := h.EarliestRecord()
+	return nil, fmt.Errorf(
+		"runtime %s round %u earlier than earliest history record %s start round %u",
+		runtime,
+		round,
+		earliestRecord.ArchiveName,
+		earliestRecord.RuntimeStartRounds[runtime],
+	)
+}
+
 func SingleRecordHistory(chainContext string) *History {
 	return &History{
 		Records: []*Record{
@@ -61,7 +87,11 @@ var DefaultChains = map[string]*History{
 				// https://github.com/oasisprotocol/mainnet-artifacts/releases/tag/2022-04-11
 				ArchiveName:   "damask",
 				GenesisHeight: 8048956,
-				ChainContext:  "b11b369e0da5bb230b220127f5e7b242d385ef8c6f54906243f30af63c815535",
+				RuntimeStartRounds: map[common.Runtime]uint64{
+					common.RuntimeCipher:  8284,
+					common.RuntimeEmerald: 1003298,
+				},
+				ChainContext: "b11b369e0da5bb230b220127f5e7b242d385ef8c6f54906243f30af63c815535",
 			},
 			{
 				// https://github.com/oasisprotocol/mainnet-artifacts/releases/tag/2021-04-28
@@ -97,6 +127,10 @@ var DefaultChains = map[string]*History{
 				// https://github.com/oasisprotocol/testnet-artifacts/releases/tag/2022-03-03
 				ArchiveName:   "2022-03-03",
 				GenesisHeight: 8535081,
+				RuntimeStartRounds: map[common.Runtime]uint64{
+					common.RuntimeCipher:  1675996,
+					common.RuntimeEmerald: 398623,
+				},
 			},
 			{
 				// https://github.com/oasisprotocol/testnet-artifacts/releases/tag/2021-04-13
