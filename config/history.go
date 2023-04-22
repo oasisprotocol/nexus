6f18792e4639faddce8f9de3a813889ecc8dc064
Warren He <w@oasislabs.com>
2023-04-21 17:51:37 -0700

    config: add explicit zero entries to RuntimeStartRounds

--- config/history.go
+++ config/history.go
@@ -14,7 +14,10 @@ type Record struct {
 	GenesisHeight int64  `koanf:"genesis_height"`
 	// RuntimeStartRounds has entries for runtimes that already exist at the
 	// genesis of this network. Look these up in the genesis document's
-	// .roothash.runtime_states[runtime_id_hex].round
+	// .roothash.runtime_states[runtime_id_hex].round. For clarity, add an
+	// entry stating round 0 in the first network where a runtime is available
+	// (although code does not differentiate between the presence or absence
+	// of a zero entry).
 	RuntimeStartRounds map[common.Runtime]uint64 `koanf:"runtime_start_rounds"`
 	ChainContext       string                    `koanf:"chain_context"`
 }
@@ -88,8 +91,9 @@ var DefaultChains = map[string]*History{
 				ArchiveName:   "damask",
 				GenesisHeight: 8048956,
 				RuntimeStartRounds: map[common.Runtime]uint64{
-					common.RuntimeCipher:  8284,
-					common.RuntimeEmerald: 1003298,
+					common.RuntimeCipher:   8284,
+					common.RuntimeEmerald:  1003298,
+					common.RuntimeSapphire: 0,
 				},
 				ChainContext: "b11b369e0da5bb230b220127f5e7b242d385ef8c6f54906243f30af63c815535",
 			},
@@ -97,7 +101,11 @@ var DefaultChains = map[string]*History{
 				// https://github.com/oasisprotocol/mainnet-artifacts/releases/tag/2021-04-28
 				ArchiveName:   "cobalt",
 				GenesisHeight: 3027601,
-				ChainContext:  "53852332637bacb61b91b6411ab4095168ba02a50be4c3f82448438826f23898",
+				RuntimeStartRounds: map[common.Runtime]uint64{
+					common.RuntimeCipher:  0,
+					common.RuntimeEmerald: 0,
+				},
+				ChainContext: "53852332637bacb61b91b6411ab4095168ba02a50be4c3f82448438826f23898",
 			},
 			{
 				// https://github.com/oasisprotocol/mainnet-artifacts/releases/tag/2020-11-18
@@ -128,14 +136,19 @@ var DefaultChains = map[string]*History{
 				ArchiveName:   "2022-03-03",
 				GenesisHeight: 8535081,
 				RuntimeStartRounds: map[common.Runtime]uint64{
-					common.RuntimeCipher:  1675996,
-					common.RuntimeEmerald: 398623,
+					common.RuntimeCipher:   1675996,
+					common.RuntimeEmerald:  398623,
+					common.RuntimeSapphire: 0,
 				},
 			},
 			{
 				// https://github.com/oasisprotocol/testnet-artifacts/releases/tag/2021-04-13
 				ArchiveName:   "2021-04-13",
 				GenesisHeight: 3398334,
+				RuntimeStartRounds: map[common.Runtime]uint64{
+					common.RuntimeCipher:  0,
+					common.RuntimeEmerald: 0,
+				},
 			},
 			{
 				// https://github.com/oasisprotocol/testnet-artifacts/releases/tag/2021-03-24
