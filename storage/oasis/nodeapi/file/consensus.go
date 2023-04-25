8cd781c81aca4d7b1685c1538df4da6a191230ce
Mitja T <mitjat@oasislabs.com>
2023-04-25 01:41:13 -0700

    Remove unnecessary params

--- storage/oasis/nodeapi/file/consensus.go
+++ storage/oasis/nodeapi/file/consensus.go
@@ -35,51 +35,51 @@ func NewFileConsensusApiLite(filename string, consensusApi nodeapi.ConsensusApiL
 	}, nil
 }
 
-// fetchFromCacheOrElseCall fetches the value of `cacheKey` from the cache if it exists,
-// interpreted as a `Response`. If it does not exist, it calls `backingApiFunc` to get the
+// getFromCacheOrCall fetches the value of `cacheKey` from the cache if it exists,
+// interpreted as a `Value`. If it does not exist, it calls `valueFunc` to get the
 // value, and caches it before returning it.
-// `height` is taken as an explicit parameter to catch non-cacheable calls. If `backingApiFunc`
+// `height` is taken as an explicit parameter to catch non-cacheable calls. If `valueFunc`
 // is not height-based, `height` should be set to `nil`.
-func fetchFromCacheOrElseCall[Response any](c *FileConsensusApiLite, ctx context.Context, height *int64, cacheKey []byte, backingApiFunc func() (*Response, error)) (*Response, error) {
+func getFromCacheOrCall[Value any](cache pogreb.DB, height *int64, cacheKey []byte, valueFunc func() (*Value, error)) (*Value, error) {
 	// If the latest height was requested, the response is not cacheable, so we have to hit the backing API.
 	if height != nil && *height == consensus.HeightLatest {
-		return backingApiFunc()
+		return valueFunc()
 	}
 
 	// If the value is cached, return it.
-	isCached, err := c.db.Has(cacheKey)
+	isCached, err := cache.Has(cacheKey)
 	if err != nil {
 		return nil, err
 	}
 	if isCached {
-		raw, err := c.db.Get(cacheKey)
+		raw, err := cache.Get(cacheKey)
 		if err != nil {
 			return nil, err
 		}
-		var result *Response
+		var result *Value
 		err = cbor.Unmarshal(raw, &result)
 		return result, err
 	}
 
 	// Otherwise, the value is not cached. Call the backing API to get it.
-	result, err := backingApiFunc()
+	result, err := valueFunc()
 	if err != nil {
 		return nil, err
 	}
 
 	// Store value in cache for later use.
-	return result, c.db.Put(cacheKey, cbor.Marshal(result))
+	return result, cache.Put(cacheKey, cbor.Marshal(result))
 }
 
-// Like fetchFromCacheOrElseCall, but for slice-typed return values.
-func fetchSliceFromCacheOrElseCall[Response any](c *FileConsensusApiLite, ctx context.Context, height *int64, cacheKey []byte, backingApiFunc func() ([]Response, error)) ([]Response, error) {
-	// Use `fetchFromCacheOrElseCall()` to avoid duplicating the cache update logic.
-	responsePtr, err := fetchFromCacheOrElseCall(c, ctx, height, cacheKey, func() (*[]Response, error) {
-		response, err := backingApiFunc()
+// Like getFromCacheOrCall, but for slice-typed return values.
+func getSliceFromCacheOrCall[Response any](cache pogreb.DB, height *int64, cacheKey []byte, valueFunc func() ([]Response, error)) ([]Response, error) {
+	// Use `getFromCacheOrCall()` to avoid duplicating the cache update logic.
+	responsePtr, err := getFromCacheOrCall(cache, height, cacheKey, func() (*[]Response, error) {
+		response, err := valueFunc()
 		if response == nil {
 			return nil, err
 		}
-		// Return the response wrapped in a pointer to conform to the signature of `fetchFromCacheOrElseCall()`.
+		// Return the response wrapped in a pointer to conform to the signature of `getFromCacheOrCall()`.
 		return &response, err
 	})
 	if responsePtr == nil {
@@ -90,32 +90,32 @@ func fetchSliceFromCacheOrElseCall[Response any](c *FileConsensusApiLite, ctx co
 }
 
 func (c *FileConsensusApiLite) GetGenesisDocument(ctx context.Context) (*genesis.Document, error) {
-	return fetchFromCacheOrElseCall(
-		c, ctx, nil, /* height */
+	return getFromCacheOrCall(
+		c.db, nil, /* height */
 		generateCacheKey("GetGenesisDocument"),
 		func() (*genesis.Document, error) { return c.consensusApi.GetGenesisDocument(ctx) },
 	)
 }
 
 func (c *FileConsensusApiLite) StateToGenesis(ctx context.Context, height int64) (*genesis.Document, error) {
-	return fetchFromCacheOrElseCall(
-		c, ctx, &height,
+	return getFromCacheOrCall(
+		c.db, &height,
 		generateCacheKey("StateToGenesis", height),
 		func() (*genesis.Document, error) { return c.consensusApi.StateToGenesis(ctx, height) },
 	)
 }
 
 func (c *FileConsensusApiLite) GetBlock(ctx context.Context, height int64) (*consensus.Block, error) {
-	return fetchFromCacheOrElseCall(
-		c, ctx, &height,
+	return getFromCacheOrCall(
+		c.db, &height,
 		generateCacheKey("GetBlock", height),
 		func() (*consensus.Block, error) { return c.consensusApi.GetBlock(ctx, height) },
 	)
 }
 
 func (c *FileConsensusApiLite) GetTransactionsWithResults(ctx context.Context, height int64) ([]nodeapi.TransactionWithResults, error) {
-	return fetchSliceFromCacheOrElseCall(
-		c, ctx, &height,
+	return getSliceFromCacheOrCall(
+		c.db, &height,
 		generateCacheKey("GetTransactionsWithResults", height),
 		func() ([]nodeapi.TransactionWithResults, error) {
 			return c.consensusApi.GetTransactionsWithResults(ctx, height)
@@ -124,8 +124,8 @@ func (c *FileConsensusApiLite) GetTransactionsWithResults(ctx context.Context, h
 }
 
 func (c *FileConsensusApiLite) GetEpoch(ctx context.Context, height int64) (beacon.EpochTime, error) {
-	time, err := fetchFromCacheOrElseCall(
-		c, ctx, &height,
+	time, err := getFromCacheOrCall(
+		c.db, &height,
 		generateCacheKey("GetEpoch", height),
 		func() (*beacon.EpochTime, error) {
 			time, err := c.consensusApi.GetEpoch(ctx, height)
@@ -136,56 +136,56 @@ func (c *FileConsensusApiLite) GetEpoch(ctx context.Context, height int64) (beac
 }
 
 func (c *FileConsensusApiLite) RegistryEvents(ctx context.Context, height int64) ([]nodeapi.Event, error) {
-	return fetchSliceFromCacheOrElseCall(
-		c, ctx, &height,
+	return getSliceFromCacheOrCall(
+		c.db, &height,
 		generateCacheKey("RegistryEvents", height),
 		func() ([]nodeapi.Event, error) { return c.consensusApi.RegistryEvents(ctx, height) },
 	)
 }
 
 func (c *FileConsensusApiLite) StakingEvents(ctx context.Context, height int64) ([]nodeapi.Event, error) {
-	return fetchSliceFromCacheOrElseCall(
-		c, ctx, &height,
+	return getSliceFromCacheOrCall(
+		c.db, &height,
 		generateCacheKey("StakingEvents", height),
 		func() ([]nodeapi.Event, error) { return c.consensusApi.StakingEvents(ctx, height) },
 	)
 }
 
 func (c *FileConsensusApiLite) GovernanceEvents(ctx context.Context, height int64) ([]nodeapi.Event, error) {
-	return fetchSliceFromCacheOrElseCall(
-		c, ctx, &height,
+	return getSliceFromCacheOrCall(
+		c.db, &height,
 		generateCacheKey("GovernanceEvents", height),
 		func() ([]nodeapi.Event, error) { return c.consensusApi.GovernanceEvents(ctx, height) },
 	)
 }
 
 func (c *FileConsensusApiLite) RoothashEvents(ctx context.Context, height int64) ([]nodeapi.Event, error) {
-	return fetchSliceFromCacheOrElseCall(
-		c, ctx, &height,
+	return getSliceFromCacheOrCall(
+		c.db, &height,
 		generateCacheKey("RoothashEvents", height),
 		func() ([]nodeapi.Event, error) { return c.consensusApi.RoothashEvents(ctx, height) },
 	)
 }
 
 func (c *FileConsensusApiLite) GetValidators(ctx context.Context, height int64) ([]nodeapi.Validator, error) {
-	return fetchSliceFromCacheOrElseCall(
-		c, ctx, &height,
+	return getSliceFromCacheOrCall(
+		c.db, &height,
 		generateCacheKey("GetValidators", height),
 		func() ([]nodeapi.Validator, error) { return c.consensusApi.GetValidators(ctx, height) },
 	)
 }
 
 func (c *FileConsensusApiLite) GetCommittees(ctx context.Context, height int64, runtimeID coreCommon.Namespace) ([]nodeapi.Committee, error) {
-	return fetchSliceFromCacheOrElseCall(
-		c, ctx, &height,
+	return getSliceFromCacheOrCall(
+		c.db, &height,
 		generateCacheKey("GetCommittee", height, runtimeID),
 		func() ([]nodeapi.Committee, error) { return c.consensusApi.GetCommittees(ctx, height, runtimeID) },
 	)
 }
 
 func (c *FileConsensusApiLite) GetProposal(ctx context.Context, height int64, proposalID uint64) (*nodeapi.Proposal, error) {
-	return fetchFromCacheOrElseCall(
-		c, ctx, &height,
+	return getFromCacheOrCall(
+		c.db, &height,
 		generateCacheKey("GetProposal", height, proposalID),
 		func() (*nodeapi.Proposal, error) { return c.consensusApi.GetProposal(ctx, height, proposalID) },
 	)
