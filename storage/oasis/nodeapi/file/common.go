212adb5c634284980d49c80dfe3733112148ad0d
Mitja T <mitjat@oasislabs.com>
2023-04-25 17:21:28 -0700

    KVStore: Encode keys with cbor, not gob

--- storage/oasis/nodeapi/file/common.go
+++ storage/oasis/nodeapi/file/common.go
@@ -1,8 +1,6 @@
 package file
 
 import (
-	"bytes"
-	"encoding/gob"
 	"errors"
 
 	"github.com/akrylysov/pogreb"
@@ -15,19 +13,7 @@ type NodeApiMethod func() (interface{}, error)
 var ErrUnstableRPCMethod = errors.New("this method is not cacheable because the RPC return value is not constant")
 
 func generateCacheKey(methodName string, params ...interface{}) []byte {
-	var buf bytes.Buffer
-	enc := gob.NewEncoder(&buf)
-	err := enc.Encode(methodName)
-	if err != nil {
-		panic(err)
-	}
-	for _, p := range params {
-		err = enc.Encode(p)
-		if err != nil {
-			panic(err)
-		}
-	}
-	return buf.Bytes()
+	return cbor.Marshal([]interface{}{methodName, params})
 }
 
 type KVStore struct{ *pogreb.DB }
