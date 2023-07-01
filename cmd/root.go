b88e311d0ca5e6298c0ca47002dca96d5bb26302
Mitja T <mitjat@oasislabs.com>
2023-07-01 10:26:07 -0700

    cmd: Log goroutines (stack traces) on SIGUSR1

--- cmd/root.go
+++ cmd/root.go
@@ -2,9 +2,13 @@
 package cmd
 
 import (
+	"bytes"
 	"fmt"
 	"os"
+	"os/signal"
+	"runtime/pprof"
 	"sync"
+	"syscall"
 
 	"github.com/spf13/cobra"
 
@@ -84,6 +88,9 @@ func rootMain(cmd *cobra.Command, args []string) {
 
 // Execute spawns the main entry point after handing the config file.
 func Execute() {
+	// Debug hook. If we receive SIGUSR1, dump all goroutines.
+	go dumpGoroutinesOnSignal(syscall.SIGUSR1)
+
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
@@ -98,5 +105,27 @@ func init() {
 		api.Register,
 	} {
 		f(rootCmd)
+	}
+}
+
+// Starts listening for the specified signals, and logs a dump of all
+// goroutines when the process receives one of those signals.
+func dumpGoroutinesOnSignal(signals ...os.Signal) {
+	logger := log.NewDefaultLogger("toplevel")
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, signals...)
+	logger.Info("listening for signals", "signals", signals)
+	for range c {
+		b := bytes.NewBufferString("")
+		_ = pprof.Lookup("goroutine").WriteTo(b, 1)
+		logger.Warn("USER-REQUESTED DUMP: all goroutines", "goroutines_all", b.String())
+
+		b = bytes.NewBufferString("")
+		_ = pprof.Lookup("block").WriteTo(b, 1)
+		logger.Warn("USER-REQUESTED DUMP: stack traces that led to blocking on synchronization primitives", "goroutines_block", b.String())
+
+		b = bytes.NewBufferString("")
+		_ = pprof.Lookup("mutex").WriteTo(b, 1)
+		logger.Warn("USER-REQUESTED DUMP: stack traces of holders of contended mutexes", "goroutines_mutex", b.String())
 	}
 }
