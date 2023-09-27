f3634332ad7275f97ad93ccd297c358692a84436
Andrew Low <andrew.low@oasislabs.com>
2023-09-27 15:29:28 +0800

    backfill num_transfers

--- storage/migrations/19_evm_token_num_xfers.up.sql
+++ storage/migrations/19_evm_token_num_xfers.up.sql
@@ -1,5 +1,6 @@
 BEGIN;
 
+-- Backfill num_transfers
 WITH transfers AS (
     SELECT runtime, DECODE(body ->> 'address', 'base64') AS eth_addr, COUNT(*) AS num_xfers
         FROM chain.runtime_events
@@ -18,5 +19,28 @@ UPDATE chain.evm_tokens as tokens
 	WHERE
 		tokens.runtime = transfers.runtime AND
 		tokens.token_address = preimages.address;
+
+-- Backfill total_supply
+-- We dead-reckon total_supply using evm_token_balances and use
+-- this value as a default for erc721 tokens, since they might 
+-- not support totalSupply()
+WITH total_supplies AS (
+    SELECT runtime, token_address, sum(balance) as total_supply
+        FROM chain.evm_token_balances 
+        GROUP BY runtime, token_address 
+)
+UPDATE chain.evm_tokens as tokens
+    SET
+        total_supply = total_supplies.total_supply
+    FROM total_supplies
+    WHERE
+        total_supplies.runtime = tokens.runtime AND 
+        total_supplies.token_address = tokens.token_address AND
+        tokens.token_type = 721;
+-- Re-download all tokens to update total_supply, and potentially 
+-- overwrite the dead-reckoned value
+UPDATE chain.evm_tokens
+    SET
+        last_download_round = 0;
 
 COMMIT;
