beac905aa5c797438faee4e43b5a3acd7d4a552b
Warren He <w@oasislabs.com>
2023-10-16 14:59:32 -0700

    evm: add uint8 array tests

--- api/logic/test_contracts/Varied.sol
+++ api/logic/test_contracts/Varied.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
 
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.21;
 
 struct O {
     uint16 n;
@@ -21,9 +21,11 @@ interface Varied {
         address a,
         function (uint16) external returns (uint16) f,
         uint16[2] calldata xy,
+        uint8[2] calldata xy8,
         bytes calldata buf,
         string calldata s,
         uint16[] calldata l,
+        uint8[] calldata l8,
         O calldata o
     ) external;
 }
