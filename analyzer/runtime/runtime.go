e19239d6fa4cdd39512d5015c84af092e999e18f
Mitja T <mitjat@oasislabs.com>
2023-11-21 19:22:33 -0800

    metrics: Add timings for data fetch, data analysis (runtimes)

--- analyzer/runtime/runtime.go
+++ analyzer/runtime/runtime.go
@@ -137,6 +137,7 @@ func (m *processor) FinalizeFastSync(ctx context.Context, lastFastSyncHeight int
 // Implements BlockProcessor interface.
 func (m *processor) ProcessBlock(ctx context.Context, round uint64) error {
 	// Fetch all data.
+	fetchTimer := m.metrics.BlockFetchLatencies()
 	blockHeader, err := m.source.GetBlockHeader(ctx, round)
 	if err != nil {
 		if strings.Contains(err.Error(), "roothash: block not found") {
@@ -152,8 +153,10 @@ func (m *processor) ProcessBlock(ctx context.Context, round uint64) error {
 	if err != nil {
 		return err
 	}
+	fetchTimer.ObserveDuration() // We make no observation in case of a data fetch error; those timings are misleading.
 
 	// Preprocess data.
+	analysisTimer := m.metrics.BlockAnalysisLatencies()
 	blockData, err := ExtractRound(*blockHeader, transactionsWithResults, rawEvents, m.logger)
 	if err != nil {
 		return err
@@ -163,6 +166,7 @@ func (m *processor) ProcessBlock(ctx context.Context, round uint64) error {
 	batch := &storage.QueryBatch{}
 	m.queueDbUpdates(batch, blockData)
 	m.queueAccountsEvents(batch, blockData)
+	analysisTimer.ObserveDuration()
 
 	// Update indexing progress.
 	batch.Queue(
