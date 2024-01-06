3f9dba14794e3109c7b954d05b99173d5f53dd01
Warren He <w@oasislabs.com>
2024-01-05 16:43:51 -0800

    consensus: store finalized roothash messages

--- analyzer/consensus/consensus.go
+++ analyzer/consensus/consensus.go
@@ -655,8 +655,15 @@ func (m *processor) queueRegistryEventInserts(batch *storage.QueryBatch, data *r
 }
 
 func (m *processor) queueRootHashMessageUpserts(batch *storage.QueryBatch, data *rootHashData) error {
+	finalized := map[coreCommon.Namespace]uint64{}
+	var roothashMessageEvents []nodeapi.Event
 	for _, event := range data.Events {
 		switch {
+		case event.RoothashMisc != nil:
+			switch event.Type { //nolint:gocritic,exhaustive // singleCaseSwitch, no special handling for other types
+			case apiTypes.ConsensusEventTypeRoothashFinalized:
+				finalized[event.RoothashMisc.RuntimeID] = *event.RoothashMisc.Round
+			}
 		case event.RoothashExecutorCommitted != nil:
 			runtime := RuntimeFromID(event.RoothashExecutorCommitted.RuntimeID, m.network)
 			if runtime == nil {
@@ -697,6 +704,45 @@ func (m *processor) queueRootHashMessageUpserts(batch *storage.QueryBatch, data 
 					addresses.SliceFromSet(messageData.relatedAddresses),
 				)
 			}
+		case event.RoothashMessage != nil:
+			// Save these for after we collect all roothash finalized events.
+			roothashMessageEvents = append(roothashMessageEvents, event)
+		}
+	}
+	for _, event := range roothashMessageEvents {
+		runtime := RuntimeFromID(event.RoothashMessage.RuntimeID, m.network)
+		if runtime == nil {
+			continue
+		}
+		batch.Queue(queries.ConsensusRoothashMessageFinalizeUpsert,
+			runtime,
+			finalized[event.RoothashMessage.RuntimeID],
+			event.RoothashMessage.Index,
+			event.RoothashMessage.Module,
+			event.RoothashMessage.Code,
+			nil,
+		)
+	}
+	for rtid, results := range data.LastRoundResults {
+		runtime := RuntimeFromID(rtid, m.network)
+		if runtime == nil {
+			// We shouldn't even have gathered last round results for unknown
+			// runtimes. But prevent nil-runtime inserts anyway.
+			continue
+		}
+		round, ok := finalized[rtid]
+		if !ok {
+			continue
+		}
+		for _, message := range results.Messages {
+			batch.Queue(queries.ConsensusRoothashMessageFinalizeUpsert,
+				runtime,
+				round,
+				message.Index,
+				message.Module,
+				message.Code,
+				cbor.Marshal(message.Result),
+			)
 		}
 	}
 
