96e8c99414eb1ed9771afafca63f1fea85c9169a
ptrus <peter@u-s.si>
2024-01-23 10:39:12 +0100

    statecheck: exempt accounts that are stale (to be requeried)

--- tests/statecheck/runtime_test.go
+++ tests/statecheck/runtime_test.go
@@ -2,6 +2,7 @@ package statecheck
 
 import (
 	"context"
+	"fmt"
 	"os"
 	"testing"
 
@@ -11,6 +12,7 @@ import (
 	sdkConfig "github.com/oasisprotocol/oasis-sdk/client-sdk/go/config"
 	sdkTypes "github.com/oasisprotocol/oasis-sdk/client-sdk/go/types"
 
+	"github.com/oasisprotocol/nexus/analyzer/runtime/evm"
 	common "github.com/oasisprotocol/nexus/common"
 )
 
@@ -34,6 +36,8 @@ func testRuntimeAccounts(t *testing.T, runtime common.Runtime) {
 	if _, ok := os.LookupEnv("OASIS_INDEXER_HEALTHCHECK"); !ok {
 		t.Skip("skipping test since healthcheck tests are not enabled")
 	}
+
+	schema := fmt.Sprintf("snapshot_%s", runtime)
 
 	t.Log("Initializing data stores...")
 
@@ -63,15 +67,38 @@ func testRuntimeAccounts(t *testing.T, runtime common.Runtime) {
 	}
 	t.Logf("Fetched %d account addresses", len(addresses))
 
-	acctRows, err := postgresClient.Query(ctx,
-		`SELECT account_address, balance, symbol
-			FROM snapshot.runtime_sdk_balances
-			WHERE runtime=$1`, runtime,
-	)
+	// Fetch addresses for which native balance is known to be stale.
+	// These are balances that we expect them to be invalid, so exempt them from the following test.
+	exemptRows, err := postgresClient.Query(ctx, fmt.Sprintf(`
+		SELECT account_address
+		FROM %s.stale_balances
+		WHERE runtime=$1 AND token_address=$2
+	`, schema), runtime, evm.NativeRuntimeTokenAddress)
 	require.NoError(t, err)
+	exemptAccs := make(map[string]bool)
+	defer exemptRows.Close()
+	for exemptRows.Next() {
+		var addr string
+		err = exemptRows.Scan(&addr)
+		require.NoError(t, err)
+		exemptAccs[addr] = true
+	}
+	t.Logf("Found %d exempted accounts (skipped for the test)", len(exemptAccs))
+
+	// Fetch account balances.
+	acctRows, err := postgresClient.Query(ctx, fmt.Sprintf(`
+		SELECT account_address, balance, symbol
+		FROM %s.runtime_sdk_balances
+		WHERE runtime=$1
+	`, schema), runtime)
+	require.NoError(t, err)
+	defer acctRows.Close()
 	actualAccts := make(map[string]bool)
 	var allBalances uint64
 	var balanceDiscrepancies uint64
+	var notExpectedFound uint64
+
+	// Check that the account balances are accurate.
 	for acctRows.Next() {
 		var a TestRuntimeAccount
 		err = acctRows.Scan(
@@ -79,23 +106,35 @@ func testRuntimeAccounts(t *testing.T, runtime common.Runtime) {
 			&a.Balance,
 			&a.Symbol,
 		)
-		assert.NoError(t, err)
+		require.NoError(t, err)
 
 		// Check that the account exists.
 		var actualAddr sdkTypes.Address
 		err = actualAddr.UnmarshalText([]byte(a.Address))
-		assert.NoError(t, err)
+		require.NoError(t, err)
+
+		// Skip accounts that are exempted.
+		if _, ok := exemptAccs[a.Address]; ok {
+			actualAccts[a.Address] = true
+			continue
+		}
 
 		_, ok := expectedAccts[actualAddr]
 		if !ok {
-			t.Logf("address %s found, but not expected", a.Address)
-			t.Fail()
+			// If the account is not expected (missing from oasis-node) it should have a zero balance.
+			if a.Balance.Int64() != 0 {
+				notExpectedFound++
+				t.Logf("Unexpected address '%s' found, reported balance (Nexus): %d", a.Address, a.Balance.Int64())
+				t.Fail()
+			} else {
+				allBalances++
+			}
 			continue
 		}
 
 		// Check that the account balance is accurate.
 		balances, err := oasisRuntimeClient.Accounts.Balances(ctx, uint64(height), actualAddr)
-		assert.NoError(t, err)
+		require.NoError(t, err)
 		for denom, amount := range balances.Balances {
 			if stringifyDenomination(denom, sdkPT) == a.Symbol {
 				allBalances++
@@ -106,15 +145,36 @@ func testRuntimeAccounts(t *testing.T, runtime common.Runtime) {
 		}
 		actualAccts[a.Address] = true
 	}
-	t.Logf("Number of discrepancies in account balances: %d (out of: %d)", balanceDiscrepancies, allBalances)
 
+	// Check addresses that are expected, but were not present in the snapshot.
+	var expectedNotFound uint64
+	var expectedNotFoundNonZero uint64
 	for _, addr := range addresses {
 		_, ok := actualAccts[addr.String()]
 		if !ok {
-			t.Logf("address %s expected, but not found", addr.String())
+			t.Logf("Expected address '%s' not found", addr.String())
+			expectedNotFound++
 			t.Fail()
+			balances, err := oasisRuntimeClient.Accounts.Balances(ctx, uint64(height), addr)
+			assert.NoError(t, err)
+			for denom, amount := range balances.Balances {
+				if stringifyDenomination(denom, sdkPT) == nativeTokenSymbol(sdkPT) {
+					t.Logf("Balance: %s", amount)
+					if amount.ToBigInt().Int64() > 0 {
+						expectedNotFoundNonZero++
+					}
+				}
+			}
 		}
 	}
+
+	// Report results.
+	t.Logf(`Number of discrepancies in account balances: %d (out of: %d).
+ 		- Does not include accounts that are only listed in Nexus or on the chain.
+		- Exempted accounts because of known stale balance in Nexus: %d
+	`, balanceDiscrepancies, allBalances, len(exemptAccs))
+	t.Logf("Number of unexpected addresses found in Nexus: %d", notExpectedFound)
+	t.Logf("Number of expected addresses not found in Nexus: %d (with non-zero balance: %d)", expectedNotFound, expectedNotFoundNonZero)
 }
 
 func nativeTokenSymbol(sdkPT *sdkConfig.ParaTime) string {
