7e4d89ebf4372d6d8a5c7d33d5b5de7b10c01129
Andrew Low <andrew.low@oasislabs.com>
2024-02-12 22:39:57 -0500

    update migration to recompute total sent/received

--- storage/migrations/07_runtime_accounts_stats.up.sql
+++ storage/migrations/07_runtime_accounts_stats.up.sql
@@ -7,4 +7,26 @@ ALTER TABLE chain.runtime_accounts
     ADD COLUMN total_sent UINT_NUMERIC NOT NULL DEFAULT 0,
     ADD COLUMN total_received UINT_NUMERIC NOT NULL DEFAULT 0;
 
+WITH agg AS (
+    SELECT runtime, sender, sum(amount) AS total_sent
+    FROM chain.runtime_transfers
+    WHERE sender IS NOT NULL
+    GROUP BY 1, 2
+)
+INSERT INTO chain.runtime_accounts as accts (runtime, address, total_sent)
+    SELECT runtime, sender, total_sent FROM agg
+    ON CONFLICT (runtime, address) DO UPDATE
+        SET total_sent = EXCLUDED.total_sent;
+
+WITH agg AS (
+    SELECT runtime, receiver, sum(amount) AS total_received
+    FROM chain.runtime_transfers
+    WHERE receiver IS NOT NULL
+    GROUP BY 1, 2
+)
+INSERT INTO chain.runtime_accounts as accts (runtime, address, total_received)
+    SELECT runtime, receiver, total_received FROM agg
+    ON CONFLICT (runtime, address) DO UPDATE
+        SET total_received = EXCLUDED.total_received;
+
 COMMIT;
