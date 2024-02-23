880eea054c30c7b5dd1fc3ab6cae6e4c63ee0486
Warren He <w@oasislabs.com>
2024-02-22 17:24:10 -0800

    consensus: index node consensus pubkey address

--- analyzer/consensus/genesis.go
+++ analyzer/consensus/genesis.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/oasisprotocol/oasis-core/go/common/cbor"
 	"github.com/oasisprotocol/oasis-core/go/common/entity"
+	"github.com/oasisprotocol/oasis-core/go/consensus/cometbft/crypto"
 
 	"github.com/oasisprotocol/nexus/analyzer/queries"
 	"github.com/oasisprotocol/nexus/common"
@@ -119,6 +120,7 @@ func (mg *GenesisProcessor) addRegistryBackendMigrations(batch *storage.QueryBat
 			node.P2P.ID.String(),
 			nil,
 			node.Consensus.ID.String(),
+			crypto.PublicKeyToCometBFT(common.Ptr(node.Consensus.ID)).Address().String(),
 			nil,
 			nil,
 			node.Roles.String(),
