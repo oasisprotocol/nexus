e6c2de02e95e6f426b75cb68daddc0203400f93f
Warren He <w@oasislabs.com>
2024-02-23 17:08:19 -0800

    storage: add block metadata fields

---
+++ storage/migrations/19_block_meta.up.sql
@@ -0,0 +1,19 @@
+BEGIN;
+
+ALTER TABLE chain.nodes
+    ADD COLUMN consensus_pubkey_address TEXT;
+
+CREATE INDEX ix_nodes_consensus_pubkey_address ON chain.nodes (consensus_pubkey_address);
+
+ALTER TABLE chain.blocks
+    ALTER COLUMN block_hash DROP NOT NULL,
+    ALTER COLUMN time DROP NOT NULL,
+    ALTER COLUMN num_txs DROP NOT NULL,
+    ALTER COLUMN namespace DROP NOT NULL,
+    ALTER COLUMN version DROP NOT NULL,
+    ALTER COLUMN type DROP NOT NULL,
+    ALTER COLUMN root_hash DROP NOT NULL,
+    ADD COLUMN proposer_node_consensus_pubkey_address TEXT,
+    ADD COLUMN signer_node_consensus_pubkey_addresses TEXT[];
+
+COMMIT;
