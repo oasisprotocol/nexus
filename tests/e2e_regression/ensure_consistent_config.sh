2c4f68f84245fefd9dbe6da605bbe79ef2c3cde1
Andrew Low <andrew.low@oasislabs.com>
2024-03-07 17:11:08 -0800

    workflow nits
    remove old sourcify cache
    
    makefile script fix(?)
    
    fix script
    
    [do not merge] zsh-compatible version of run.sh
    
    evmverifier: remove extraneous fields
    
    add changelog

--- tests/e2e_regression/ensure_consistent_config.sh
+++ tests/e2e_regression/ensure_consistent_config.sh
@@ -19,7 +19,7 @@ shopt -s expand_aliases
 alias yaml2json="python3 -c 'import sys,yaml,json; print(json.dumps(yaml.safe_load(str(sys.stdin.read()))))'" 
 
 # Compare
-for range in $(find "$directory" -type d); do
+for range in $(find "tests/e2e_regression" -maxdepth 1 -mindepth 1 -type d); do
     cat "${range}/e2e_config_1.yml" | yaml2json | jq "$important_attrs" > /tmp/e2e_config_1.summary
     cat "${range}/e2e_config_2.yml" | yaml2json | jq "$important_attrs" > /tmp/e2e_config_2.summary
     diff /tmp/e2e_config_1.summary /tmp/e2e_config_2.summary || { echo "The two config files for e2e tests differ in key parameters! See diff above."; exit 1; }
