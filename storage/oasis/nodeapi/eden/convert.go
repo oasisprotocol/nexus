ead73b51655c7fbaa43d86788c2ea2dcfccd26a9
Warren He <w@oasislabs.com>
2024-03-18 12:28:33 -0700

    storage: add eden debond end time

--- storage/oasis/nodeapi/eden/convert.go
+++ storage/oasis/nodeapi/eden/convert.go
@@ -197,7 +197,8 @@ func ConvertGenesis(d genesisEden.Document) (*genesis.Document, error) {
 			debondingDelegations[k][k2] = make([]*staking.DebondingDelegation, len(v2))
 			for i, v3 := range v2 {
 				debondingDelegations[k][k2][i] = &staking.DebondingDelegation{
-					Shares: v3.Shares,
+					Shares:        v3.Shares,
+					DebondEndTime: v3.DebondEndTime,
 				}
 			}
 		}
