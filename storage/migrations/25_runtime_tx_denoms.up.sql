3ade9e428b343b2a97d1ed0a7c939cdabf4fa07a
Andrew Low <andrew.low@oasislabs.com>
2024-08-14 00:08:08 -0400

    fix fee_symbol column creation for stg deploy

--- storage/migrations/25_runtime_tx_denoms.up.sql
+++ storage/migrations/25_runtime_tx_denoms.up.sql
@@ -4,6 +4,6 @@ ALTER TABLE chain.runtime_transactions
     ADD COLUMN amount_symbol TEXT;
 
 ALTER TABLE chain.runtime_transactions
-    ADD COLUMN fee_symbol TEXT;
+    ADD COLUMN fee_symbol TEXT NOT NULL DEFAULT ''; -- Will be populated after reindex
 
 COMMIT;
--- storage/migrations/30_default_symbol.up.sql
+++
@@ -1,9 +0,0 @@
-BEGIN;
-
-UPDATE chain.runtime_transactions SET fee_symbol = '' WHERE fee_symbol IS NULL; -- Will be populated on reindex.
-
-ALTER TABLE chain.runtime_transactions ALTER COLUMN fee_symbol SET NOT NULL;
-
-ALTER TABLE chain.runtime_transactions ALTER COLUMN fee_symbol SET DEFAULT '';
-
-COMMIT;
