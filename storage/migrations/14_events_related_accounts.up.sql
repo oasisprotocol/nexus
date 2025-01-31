03f133295b7c6f300186298bc10cbdd05ebd465d
ptrus <peter@u-s.si>
2025-01-31 20:06:39 +0100

    migration optimizations

--- storage/migrations/14_events_related_accounts.up.sql
+++ storage/migrations/14_events_related_accounts.up.sql
@@ -2,31 +2,22 @@ BEGIN;
 
 ALTER TABLE chain.events ADD COLUMN event_index UINT31;
 
+-- Disable triggers for faster updates.
+ALTER TABLE chain.events DISABLE TRIGGER ALL;
+
 -- Populate the event_index column with sequential values for each tx_block to ensure uniqueness.
-DO $$
-DECLARE
-  cur_block UINT63;
-  cur_type TEXT;
-BEGIN
-  -- Iterate over each unique (tx_block, type) pair.
-  FOR cur_block, cur_type IN
-    SELECT tx_block, type
-    FROM chain.events
-    GROUP BY tx_block, type
-  LOOP
-    -- Assign event_index within the group.
-    WITH ranked_events AS (
-      SELECT ctid,
-             row_number() OVER (ORDER BY timestamp ASC) - 1 AS event_idx
-      FROM chain.events
-      WHERE tx_block = cur_block AND type = cur_type
-    )
-    UPDATE chain.events e
-    SET event_index = ranked_events.event_idx
-    FROM ranked_events
-    WHERE e.ctid = ranked_events.ctid;
-  END LOOP;
-END $$;
+WITH ranked_events AS (
+  SELECT ctid,
+         row_number() OVER (PARTITION BY tx_block, type) - 1 AS event_idx
+  FROM chain.events
+)
+UPDATE chain.events e
+SET event_index = ranked_events.event_idx
+FROM ranked_events
+WHERE e.ctid = ranked_events.ctid;
+
+-- Re-enable triggers.
+ALTER TABLE chain.events ENABLE TRIGGER ALL;
 
 ALTER TABLE chain.events
     ALTER COLUMN event_index SET NOT NULL;
@@ -34,7 +25,6 @@ ALTER TABLE chain.events
 -- Primary key for runtime events.
 ALTER TABLE chain.events
     ADD CONSTRAINT pk_events PRIMARY KEY (tx_block, type, event_index);
-
 
 -- Create events related accounts table.
 CREATE TABLE chain.events_related_accounts
@@ -49,21 +39,24 @@ CREATE TABLE chain.events_related_accounts
     FOREIGN KEY (tx_block, type, event_index) REFERENCES chain.events(tx_block, type, event_index) DEFERRABLE INITIALLY DEFERRED
 );
 
+-- Disable logging for faster insertion.
+ALTER TABLE chain.events_related_accounts SET UNLOGGED;
+
 -- Populate the events_related_accounts table.
-INSERT INTO
-    chain.events_related_accounts (tx_block, type, event_index, tx_index, account_address)
-SELECT
-    tx_block, type, event_index, tx_index, unnest(related_accounts) AS account_address
-FROM
-    chain.events
-WHERE
-    related_accounts IS NOT NULL
+INSERT INTO chain.events_related_accounts (tx_block, type, event_index, tx_index, account_address)
+    SELECT tx_block, type, event_index, tx_index, unnest(related_accounts)
+    FROM chain.events
+    WHERE related_accounts IS NOT NULL
     AND array_length(related_accounts, 1) > 0;
 
+
+-- Restore logging.
+ALTER TABLE chain.events_related_accounts SET LOGGED;
 
 -- Used for fetching all events related to an account (sorted by round).
 CREATE INDEX ix_events_related_accounts_account_address_block ON chain.events_related_accounts(account_address, tx_block DESC, tx_index); -- TODO: maybe also event index?
 
+-- Clean up.
 DROP INDEX chain.ix_events_related_accounts;
 ALTER TABLE chain.events DROP COLUMN related_accounts;
 
